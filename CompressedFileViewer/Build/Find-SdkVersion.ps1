fc8adc6897ea664d8ce10c2858d2a9d859bd73c1
Pascal-Krenckel <81245422+Pascal-Krenckel@users.noreply.github.com>
2026-06-03 23:01:49 +0200

    BugFix: rc.exe is part of C++ dev tools and not part of C#. Manually search for rc.exe. Also: Replacing hard coded entries of rc-script with defines.

---
+++ CompressedFileViewer/Build/Find-SdkVersion.ps1
@@ -0,0 +1,17 @@
+﻿$kitsRoot = (Get-ItemProperty `
+    -Path "HKLM:\SOFTWARE\Microsoft\Windows Kits\Installed Roots" `
+    -Name "KitsRoot10").KitsRoot10
+
+$binRoot = Join-Path $kitsRoot "bin"
+
+$version = Get-ChildItem $binRoot -Directory |
+    Where-Object { $_.Name -match '^\d+\.\d+\.\d+\.\d+$' } |
+    Sort-Object Name -Descending |
+    Select-Object -First 1
+
+if ($version) {
+    Write-Output $version.Name
+    exit 0
+}
+
+exit 1
